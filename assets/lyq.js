b2699f9382e6983132e3e7c0e519eb81a5b60ce7
myzhang1029 <myzhang1029@163.com>
2019-06-28 17:23:07 +0800

    Better javascript

--- assets/lyq.js
+++ assets/lyq.js
@@ -1,33 +1,44 @@
 // lyq.js: formatting the laoyin quotes and controlling the page functions
+
 function setCookie(key, value) {
   var expires = new Date();
   expires.setTime(expires.getTime() + (1 * 365 * 24 * 60 * 60 * 1000));
   document.cookie = key + '=' + value +';path=/'+ ';expires=' + expires.toUTCString();
 }
+
 function getCookie(key) {
   var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
   return keyValue ? keyValue[2] : null;
 }
+
+/* Automatically resize every parameter for .masthead when something resizes */
 function resizeTitleBar() {
-  /* The width, left and right margin and padding are the same as the container */
+  /* The width, left and right margins are the same as the container */
   $("#notmoving").css("width", $(".container").innerWidth());
   $("#notmoving").css("margin-left", $(".container").css("margin-left"));
-  $("#notmoving").css("padding-left", $(".container").css("padding-left"));
   $("#notmoving").css("margin-right", $(".container").css("margin-right"));
-  $("#notmoving").css("padding-right", $(".container").css("padding-right"));
+  /* This box should be adjacent to #notmoving */
+  /* $.css() returns "npx", so we parseInt */
   $("#dropdown").css("width", $(".container").innerWidth() - 2 * parseInt($(".container").css("padding-right")));
-  $("#dropdown").css("left", $(".container").css("padding-left"));
   $("#dropdown").css("margin-left", $(".container").css("margin-left"));
-  $(".masthead").css("height", $("#notmoving").outerHeight(true) + $("#dropdown").outerHeight(true) + 2 * parseInt($(".masthead").css("padding-top")));
+  /* Due to some reasons, css max-height needs to be dynamically set */
+  $("#dropdown").css("max-height", $(window).outerHeight(true) * 0.3);
+  /* Make sure .masthead can cover all of #notmoving and #dropdown */
+  $(".masthead").css("height", $("#notmoving").outerHeight(true) +
+  $("#dropdown").outerHeight(true) + parseInt($(".masthead").css("padding-top")) +
+  parseInt($(".masthead").css("padding-bottom")));
+  /* Inherit all margins from .container */
   $(".masthead").css("width", $(".container").innerWidth());
   $(".masthead").css("margin-left", $(".container").css("margin-left"));
-  $(".masthead").css("padding-left", $(".container").css("padding-left"));
   $(".masthead").css("margin-right", $(".container").css("margin-right"));
-  $(".masthead").css("padding-right", $(".container").css("padding-right"));
+  /* Move main and footer down */
   $("main").css("top", $(".masthead").outerHeight(true));
   $("footer").css("top", $(".masthead").outerHeight(true));
+  /* Extend $(document) so that moved contents won't be cropped */
   $("#adjustheight").height($(".masthead").outerHeight(true));
 }
+
+/* Display a thicker bar under selected tab */
 function checkVisibility() {
   if ($("#toc").is(":visible")) {
     $("#toggletoc").css("border-bottom", "3px #313131 solid");
@@ -47,6 +58,7 @@ function checkVisibility() {
   /* The size of .masthead must have changed */
   resizeTitleBar();
 }
+/* Control the tabs */
 function toggleToc() {
   $('#sitemenu').hide();
   $('#options').hide();
@@ -66,11 +78,27 @@ $(document).ready(function() {
   $('#toc').toc({
     listType: 'ul',
     title: '',
-    headers: 'h1'
+    minimumHeaders: 1,
+    headers: 'h1',
+    classes: {
+      item: "toc_li"
+    }
   });
+  /* Collape the toc when an anchor is clicked */
+  $(".toc_li").each(function(i) {
+    $(this).mouseup(function() {
+      toggleToc();
+      /* Scroll down a bit */
+      setTimeout(function() {
+        $(document).scrollTop($(document).scrollTop() -
+        $("#adjustheight").outerHeight(true));
+      }, 100);
+    });
+  })
   $('#toc').hide();
   $('#sitemenu').hide();
   $('#options').hide();
+  /* Restore defaults */
   if (getCookie("showlyq") == "false") {
     $("#showlyq").click();
   }
@@ -86,19 +114,32 @@ $(document).ready(function() {
   if (getCookie("showfinal") == "true") {
     $("#showfinal").click();
   }
-  /* Set the .masthead to be fixed to the top */
+  /* Set .masthead to be fixed to the top */
   $(".masthead").css("position", "fixed");
   $(".masthead").css("top", "0px");
   $(".masthead").css("left", "0px");
   $(".masthead").css("z-index", "98");
+  /* Set #notmoving to be fixed to the top */
   $("#notmoving").css("position", "fixed");
+  /* Moe it down a little bit so that it doesn't stick at the very top */
   $("#notmoving").css("top", $(".masthead").css("padding-top"));
+  /* It has padding-left */
   $("#notmoving").css("left", "0px");
+  /* Higher than .masthead */
   $("#notmoving").css("z-index", "99");
   $("#dropdown").css("position", "fixed");
-  $("#dropdown").css("top", parseInt($(".masthead").css("padding-top")) + $("#notmoving").outerHeight(true) + 1.24);
-  $("#dropdown").css("left", "0px");
+  /* Lower than #notmoving */
+  /* It the screen width change across $large-breakpoint, the behavoir is still
+  weird, but it doesn't matter */
+  $("#dropdown").css("top", parseInt($(".masthead").css("padding-top")) + $("#notmoving").outerHeight(true) + 1.24/* magic */);
+  $("#dropdown").css("left", $(".container").css("padding-left"));
   $("#dropdown").css("z-index", "99");
+  /* These values don't change */
+  $("#notmoving").css("padding-left", $(".container").css("padding-left"));
+  $("#notmoving").css("padding-right", $(".container").css("padding-right"));
+  $(".masthead").css("padding-left", $(".container").css("padding-left"));
+  $(".masthead").css("padding-right", $(".container").css("padding-right"));
+  /* Move down other contents */
   $("main").css("position", "relative");
   $("footer").css("position", "relative");
   resizeTitleBar();
